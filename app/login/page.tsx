9209fae3ab645fc5db19a1e44d4e7344704bf9e8
Victor <victoorres@icloud.com>
2025-05-06 21:41:46 -0300

    feat: adicionando rota backend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -94,7 +94,7 @@ export default function LoginPage() {
     setLoginError(null);
 
     try {
-      const response = await fetch('http://localhost:8080/api/auth/login', {
+      const response = await fetch('https://1b5d18bb-1e08-45d0-951f-b4b56bfbb21d-00-12roaffq8bd2j.riker.replit.dev:8000//api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -163,7 +163,7 @@ export default function LoginPage() {
 
     const isProfessional = registerType === 'professional';
     const data = isProfessional ? professionalData : partnerSupplierData;
-    const url = `http://localhost:8080/api/${isProfessional ? 'professional' : 'partner-supplier'}`;
+    const url = `https://1b5d18bb-1e08-45d0-951f-b4b56bfbb21d-00-12roaffq8bd2j.riker.replit.dev/api/${isProfessional ? 'professional' : 'partner-supplier'}`;
 
     if (data.password !== data.confirmPassword) {
       toast.error('As senhas não coincidem.');
